c43284a0de516f56bcda204e65c0f1c0c386bc51
Vincent Viallet <vincent@wiredcraft.com>
2013-05-22 17:06:40 +0800

    Update axon event and message format.

--- lib/ansible/callback_plugins/axon-sender.py
+++ lib/ansible/callback_plugins/axon-sender.py
@@ -27,7 +27,7 @@ def record(result):
         result['space'] = os.environ.get('ANSIBLE_DEVOPS_SPACE', '')
         try: 
             sock.connect((AXON_HOST, AXON_PORT))
-            sock.push('log', result)
+            sock.push('ansible', result)
         except Exception, e: 
             # send to logs
             print "Error; playbook callback can not send Axon message - %s" % e
@@ -39,73 +39,70 @@ class CallbackModule(object):
         pass
 
     def playbook_on_start(self):
-        record({'type': LVL_NOTI, 'message': 'Start playbook'})
+        record({
+            'type': LVL_NOTI, 
+            'event': 'playbook_on_start', 
+            'msg': 'Start playbook'
+        })
 
     def runner_on_failed(self, host, res, ignore_errors=False):
         if ignore_errors:
             return
-        record({'type': LVL_ERR, 'source': host, 'message': res})
+        result = {
+            'type': LVL_ERR, 
+            'event': 'runner_on_failed', 
+            'source': host, 
+        }
+        result.update(res)
+        record(result)
 
     def runner_on_ok(self, host, res):
-        record({'type': LVL_NOTI, 'source': host, 'message': res})
+        result = {
+            'type': LVL_NOTI, 
+            'event': 'runner_on_ok', 
+            'source': host, 
+        }
+        result.update(res)
+        record(result)
 
     def runner_on_error(self, host, msg):
-        record({'type': LVL_ERR, 'source': host, 'message': msg})
+        record({
+            'type': LVL_ERR, 
+            'event': 'runner_on_error', 
+            'source': host, 
+            'msg': msg
+        })
 
     def runner_on_skipped(self, host, item=None):
-        record({'type': LVL_NOTI, 'source': host, 'message': item})
+        record({
+            'type': LVL_NOTI, 
+            'event': 'runner_on_skipped', 
+            'source': host, 
+            'item': item
+        })
 
     def runner_on_unreachable(self, host, res):
-        record({'type': LVL_ERR, 'source': host, 'message': res})
+        result = {
+            'type': LVL_ERR, 
+            'event': 'runner_on_unreachable', 
+            'source': host, 
+        }
+        result.update(res)
+        record(result)
 
     def playbook_on_stats(self, stats):
-        result = {}
         for host in stats.processed:
-            record({'type': LVL_NOTI, 'source': host, 'message': stats.summarize(host)})
-            # result[host] = stats.summarize(host)
+            record({
+                'type': LVL_NOTI, 
+                'event': 'playbook_on_stats', 
+                'source': host, 
+                'stats': stats.summarize(host)
+            })
 
-    # def runner_on_no_hosts(self):
-    #     pass
-
-    # def runner_on_async_poll(self, host, res, jid, clock):
-    #     pass
-    #     # record({'type': 'runner_on_async_poll', 'source':host, 'message':res, 'jid': jid, 'clock': clock})
-
-    # def runner_on_async_ok(self, host, res, jid):
-    #     pass
-    #     # record({'type': 'Notifications', 'source':host, 'message':res, 'jid': jid})
-
-    # def runner_on_async_failed(self, host, res, jid):
-    #     pass
-    #     # record({'type': 'Errors', 'source':host, 'message':res, 'jid': jid})
-
-    # def playbook_on_notify(self, host, handler):
-    #     pass
-    #     # record({'type': 'Notifications', 'source':host, 'handler': handler})
-
-    # def on_no_hosts_matched(self):
-    #     pass
-
-    # def on_no_hosts_remaining(self):
-    #     pass
-
-    # def playbook_on_task_start(self, name, is_conditional):
-    #     pass
-
-    # def playbook_on_vars_prompt(self, varname, private=True, prompt=None, encrypt=None, confirm=False, salt_size=None, salt=None, default=None):
-    #     pass
-    #     # record({'type': 'playbook_on_vars_prompt', 'varname': varname, 'private': private, 'prompt':prompt, 'encrypt':encrypt, 'confirm':confirm, 'salt_size':salt_size, 'salt':salt, 'default': default})
-
-    # def playbook_on_setup(self):
-    #     pass
-
-    # def playbook_on_import_for_host(self, host, imported_file):
-    #     pass
-    #     # record({'type': 'Notifications', 'source':host, 'imported_file': imported_file})
-
-    # def playbook_on_not_import_for_host(self, host, missing_file):
-    #     pass
-
-    # def playbook_on_play_start(self, pattern):
-    #     pass
-
+    def playbook_on_notify(self, host, handler):
+        record({
+            'type': LVL_NOTI, 
+            'event': 'playbook_on_notify', 
+            'source': host, 
+            'handler': handler
+        })
